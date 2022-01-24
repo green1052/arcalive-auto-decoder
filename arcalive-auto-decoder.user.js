2c9d58bcfdc1f76904afe779ac2f6a65ce61c5ee
green1052 <green1052@protonmail.ch>
2022-01-25 03:32:50 +0900

    Add files via upload

---
+++ arcalive-auto-decoder.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name arcalive-auto-decoder
+// @version 1.0.0
+// @author ㅇㅇ(69.74)
+// @description 아카라이브 base64 내용을 자동으로 디코딩합니다.
+// @match http*://arca.live/b/*/*
+// @namespace arcalive-auto-decoder
+// @rut-at document-start
+// @noframes
+// @license GPLv3
+// @homepageURL	https://github.com/green1052/arcalive-auto-decoder
+// @downloadURL https://raw.githubusercontent.com/green1052/arcalive-auto-decoder/main/arcalive-auto-decoder.user.js
+// @homepageURL https://github.com/green1052/arcalive-auto-decoder
+// @require https://unpkg.com/url-regex-safe
+// ==/UserScript==
+
+window.addEventListener("DOMContentLoaded", () => {
+    "use strict";
+
+    const content = document.querySelector("div.article-content");
+    const matches = content.innerHTML.match(/(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})/g);
+
+    for (const match of matches) {
+        try {
+            const decode = atob(match);
+
+            if (!decode || !urlRegexSafe().test(decode)) continue;
+
+            content.innerHTML = content.innerHTML.replace(match, `<a href="${decode}" target="_blank">${decode}</a>`);
+        } catch {
+        }
+    }
+});
