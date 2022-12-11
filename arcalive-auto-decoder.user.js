996e923a1300e7e310a126ff541d5121d4e99e9b
green1052 <34261355+green1052@users.noreply.github.com>
2022-12-12 05:26:24 +0900

    Update arcalive-auto-decoder.user.js

--- arcalive-auto-decoder.user.js
+++ arcalive-auto-decoder.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name arcalive-auto-decoder
-// @version 1.0.1
+// @version 2.0.0
 // @author green1052
 // @description 아카라이브 base64 내용을 자동으로 디코딩합니다.
 // @match https://arca.live/b/*/*
@@ -10,23 +10,30 @@
 // @license GPLv3
 // @downloadURL https://raw.githubusercontent.com/green1052/arcalive-auto-decoder/main/arcalive-auto-decoder.user.js
 // @homepageURL https://github.com/green1052/arcalive-auto-decoder
-// @require https://unpkg.com/url-regex-safe
 // ==/UserScript==
 
 (() => {
     "use strict";
 
     const content = document.querySelector("div.article-content");
-    const matches = content.innerHTML.match(/(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})/g);
 
-    for (const match of matches) {
-        try {
-            const decode = atob(match);
+    function decode(regex) {
+        while (regex.test(content.innerHTML)) {
+            let decoded = regex.exec(content.innerHTML)[0];
 
-            if (!decode || !urlRegexSafe().test(decode)) continue;
+            while (decoded.match(/aHR0c[0-9A-Za-z]{8,}[=]{0,2}/) == null) {
+                decoded = atob(decoded);
+            }
 
-            content.innerHTML = content.innerHTML.replace(match, `<a href="${decode}" target="_blank">${decode}</a>`);
-        } catch {
+            decoded = atob(decoded);
+            content.innerHTML = content.innerHTML.replace(regex, `<a href="${decoded}" target="_blank" rel="noreferrer">${decoded}</a>`);
         }
     }
+
+    if (content === null) return;
+
+    decode(/aHR0c[0-9A-Za-z]{20,}[=]{0,2}/); //aHR0c:1회인코딩된것.
+    decode(/YUhSMGN[0-9A-Za-z]{80,}[=]{0,2}/); //YUhSMGN:2회인코딩된것.
+    decode(/[0-9A-Za-z]{30,}[=]{1,2}/); //문자열 30회 + '=' 1~2회
+    decode(/[0-9A-Za-z]{200,}[=]{0,2}/); //문자열 200회 + '=' 0~2회
 })();
